943da0c05b350031631b8219cdf3c248f23deab5
hasan-bd <kamrulhasan570@gmail.com>
2021-06-23 23:48:28 +0600

    Put=(update/ Overwrite) A specific article on database.

--- app.js
+++ app.js
@@ -70,6 +70,19 @@ app.route('/articles')
     })
   })
 
+  .put(function(req,res){
+    Article.update(
+      {title: req.params.articleTitle},
+      { title: req.body.title,content:req.body.content},
+      {overwrite:true},
+      function(err){
+        if(!err){
+          res.send("Successfully update the artile.")
+        }
+      }
+    )
+  })
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
